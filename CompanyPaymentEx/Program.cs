bbe0ac5a980b3c589e0898f396207eab815a2d3a
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-06-19 22:40:01 -0300

    Projeto Iniciado

---
+++ CompanyPaymentEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompanyPaymentEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
